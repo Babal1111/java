5d66f7316795d6afaefb5572235c6225300481cd
babal1111 <babaldon1111@gmail.com>
2025-12-23 10:12:57 +0530

    do while

--- javaBasics/Loops.java
+++ javaBasics/Loops.java
@@ -3,29 +3,65 @@ public class Loops{
     public static void main(String args[]){
         int a = -123;
         while(a>=-129){
-            System.out.println(a);
+            System.out.print(a+" ");
             a--;
         }
-        System.out.println("\n ------------------------------- \n");
+        System.out.println();
          a = 79;
         while(a<=84){
-            System.out.println(a);
+            System.out.print(a+" ");
             a++;
         }
-        System.out.println("\n ------------------------------- \n");
+        System.out.println("");
 
         a = 0;
         while(a>=-9){
-            System.out.println(a);
+            System.out.print(a+" ");
             a--;
         }
-        System.out.println("\n ------------------------------- \n");
+        System.out.println("");
 
         a=-110;
         while(a<=-100){
-            System.out.println(a);
+            System.out.print(a+" ");
             a++;
         }
+        System.out.println("");
+
+        char c = 'A';
+        while(c<='M'){
+            System.out.print(c+" ");
+            c++;
+        }
+        System.out.println();
+        c='X';
+        while(c<='m'){
+            System.out.print(c+" ");
+            c++;
+
+        }
+        System.out.println();
+
+        System.out.println("\n -------------  DO WHILE ------------------ \n");
+        a = -111;
+        do { 
+            System.err.print(a+" ");
+            a++;
+        } while (a<=-101);
+           System.out.println();
+
+        c='s';
+        do{
+            System.out.print(c+" ");
+            c--;
+        }while(c>='l');
+        System.out.println();
+        c='T';
+        do { 
+            System.out.print(c+" ");
+            c++;
+        } while (c<='Z');
+
 
     }
 }
